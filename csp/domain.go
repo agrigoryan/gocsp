417af290aac774f0f1f5a073e64c6477cfe2ff37
agrigoryan <aram.grigoryan@gmail.com>
2024-06-26 20:13:55 -0400

    2 versions of domain impl

--- csp/domain.go
+++ csp/domain.go
@@ -9,20 +9,20 @@ import (
 type Value int
 type ValueSet []Value
 
-type Domain struct {
-	values []Value
+type DomainWithBitmap struct {
+	values ValueSet
 	bitmap *bitset.BitSet
 }
 
-func (d *Domain) Size() int {
+func (d *DomainWithBitmap) Size() int {
 	return int(d.bitmap.Count())
 }
 
-func (d *Domain) Value(idx int) Value {
+func (d *DomainWithBitmap) Value(idx int) Value {
 	return d.values[idx]
 }
 
-func (d *Domain) Range(fn func(int) bool) {
+func (d *DomainWithBitmap) Range(fn func(int) bool) {
 	for i := 0; i < len(d.values); i++ {
 		if d.bitmap.Test(uint(i)) && fn(i) {
 			return
@@ -30,7 +30,7 @@ func (d *Domain) Range(fn func(int) bool) {
 	}
 }
 
-func (d *Domain) Filter(fn func(int) bool) {
+func (d *DomainWithBitmap) Filter(fn func(int) bool) {
 	for i := 0; i < len(d.values); i++ {
 		if d.bitmap.Test(uint(i)) && !fn(i) {
 			d.bitmap.Clear(uint(i))
@@ -38,24 +38,24 @@ func (d *Domain) Filter(fn func(int) bool) {
 	}
 }
 
-func (d *Domain) Set(idx int) {
+func (d *DomainWithBitmap) Set(idx int) {
 	d.bitmap.Set(uint(idx))
 }
 
-func (d *Domain) Unset(idx int) {
+func (d *DomainWithBitmap) Unset(idx int) {
 	d.bitmap.Clear(uint(idx))
 }
 
-func (d *Domain) Contains(idx int) bool {
+func (d *DomainWithBitmap) Contains(idx int) bool {
 	return d.bitmap.Test(uint(idx))
 }
 
-func (d *Domain) UnsetAllBut(idx int) {
+func (d *DomainWithBitmap) UnsetAllBut(idx int) {
 	d.bitmap.ClearAll()
 	d.bitmap.Set(uint(idx))
 }
 
-func (d *Domain) String() string {
+func (d *DomainWithBitmap) String() string {
 	buf := bytes.Buffer{}
 	buf.WriteByte('{')
 	d.Range(func(i int) bool {
@@ -66,16 +66,109 @@ func (d *Domain) String() string {
 	return buf.String()
 }
 
-func (d *Domain) Clone() Domain {
-	return Domain{
+func (d *DomainWithBitmap) Clone() DomainWithBitmap {
+	return DomainWithBitmap{
 		values: d.values,
 		bitmap: d.bitmap.Clone(),
 	}
 }
 
-func NewDomain(values []Value) Domain {
-	return Domain{
+func NewDomainWithBitmap(values ValueSet) DomainWithBitmap {
+	return DomainWithBitmap{
 		values: values,
 		bitmap: bitset.New(uint(len(values))).SetAll(),
 	}
+}
+
+type DomainWithRemainingIndices struct {
+	values    []Value
+	remaining []int
+}
+
+func (d *DomainWithRemainingIndices) Size() int {
+	return len(d.remaining)
+}
+
+func (d *DomainWithRemainingIndices) Value(idx int) Value {
+	return d.values[idx]
+}
+
+func (d *DomainWithRemainingIndices) Range(fn func(int) bool) {
+	for i := 0; i < len(d.remaining); i++ {
+		if fn(d.remaining[i]) {
+			return
+		}
+	}
+}
+
+func (d *DomainWithRemainingIndices) Filter(fn func(int) bool) {
+	for i := 0; i < len(d.remaining); i++ {
+		if !fn(d.remaining[i]) {
+			d.Unset(d.remaining[i])
+			i--
+		}
+	}
+}
+
+func (d *DomainWithRemainingIndices) Set(idx int) {
+	d.remaining = append(d.remaining, idx)
+}
+
+func (d *DomainWithRemainingIndices) Unset(idx int) {
+	rIdx := -1
+	for i := 0; i < len(d.remaining); i++ {
+		if d.remaining[i] == idx {
+			rIdx = i
+			break
+		}
+	}
+	if rIdx == -1 {
+		return
+	}
+	d.remaining[len(d.remaining)-1], d.remaining[rIdx] = d.remaining[rIdx], d.remaining[len(d.remaining)-1]
+	d.remaining = d.remaining[:len(d.remaining)-1]
+}
+
+func (d *DomainWithRemainingIndices) Contains(idx int) bool {
+	for i := 0; i < len(d.remaining); i++ {
+		if d.remaining[i] == idx {
+			return true
+		}
+	}
+	return false
+}
+
+func (d *DomainWithRemainingIndices) String() string {
+	buf := bytes.Buffer{}
+	buf.WriteByte('{')
+	d.Range(func(i int) bool {
+		buf.WriteString(strconv.Itoa(int(d.values[i])))
+		return false
+	})
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (d *DomainWithRemainingIndices) Clone() DomainWithRemainingIndices {
+	return DomainWithRemainingIndices{
+		values:    d.values,
+		remaining: d.remaining,
+	}
+}
+
+func NewDomainWithRemainingIndices(values ValueSet) DomainWithRemainingIndices {
+	remaining := make([]int, len(values))
+	for i := 0; i < len(values); i++ {
+		remaining[i] = i
+	}
+	return DomainWithRemainingIndices{
+		values:    values,
+		remaining: remaining,
+	}
+}
+
+type Domain = DomainWithRemainingIndices
+
+func NewDomain(values ValueSet) Domain {
+	return NewDomainWithRemainingIndices(values)
 }
